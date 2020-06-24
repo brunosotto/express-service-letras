f8105364011a4365fc8c142f16c199b3928f7f43
Bruno Sotto <bruno@sotto.com.br>
2020-06-24 20:57:38 -0300

    ultima alteração

--- src/models/louvor.model.ts
+++ src/models/louvor.model.ts
@@ -2,27 +2,32 @@ export interface ILouvor {
   id?: string;
   name: string;
   text: string;
+  arr: string[];
 }
 
 export class Louvor implements ILouvor {
   public id?: string;
   public name: string;
   public text: string;
+  public arr: string[];
 
   constructor(
     nameOrLouvor: string | ILouvor,
     text?: string,
+    arr?: string[],
   ) {
     if (typeof nameOrLouvor === 'string') {
 
       this.name = nameOrLouvor;
       this.text = text;
+      this.arr = arr;
 
     } else {
 
       this.id = nameOrLouvor.id;
       this.name = nameOrLouvor.name;
       this.text = nameOrLouvor.text;
+      this.arr = nameOrLouvor.arr;
 
     }
   }
